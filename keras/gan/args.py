be320af1343d2941859d75f5284c5592af2443ff
kotarouetake <ktro310115@gmail.com>
2020-06-26 07:36:23 +0900

    Fixed typo

--- keras/gan/args.py
+++ keras/gan/args.py
@@ -7,7 +7,7 @@ def argument_parser():
 
     parser.add_argument('-d', '--dataset', type=str, required=True, help='mnist, fashion_mnist or cifar10')
     parser.add_argument('-b', '--batchsize', type=int, default=128, help='batch size')
-    parser.add_argument('--eopch', type=int, default=20, help='epochs')
+    parser.add_argument('--epoch', type=int, default=20, help='epochs')
     parser.add_argument('--latentdim', type=int, default=100, help='latent dimention')
     parser.add_argument('--leakyrelu', type=bool, default=True, help='use leakyrelu or not')
     parser.add_argument('--tilt', type=float, default=0.2, help='tilt of leakyrelu')
